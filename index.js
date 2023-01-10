cbcd6dfa8eac11f789925dc3ef269aea5347ca03
Zach <zachlandis91@gmail.com>
2023-01-10 10:17:48 -0700

    commit tree 3053b34242950cc04befbb1ea3e9cb8779477bee

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const splitWord = word.split('')
+  const reverseArray = splitWord.reverse()
+  const reverseWord = reverseArray.join('')
+
+  if (word === reverseWord) {
+    return true 
+  } else {
+    return false
+  }
 }
+
+isPalindrome("mom")
 
 /* 
   Add your pseudocode here
@@ -11,15 +21,15 @@ function isPalindrome(word) {
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+//   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
 
-module.exports = isPalindrome;
+// module.exports = isPalindrome;
